80075255450d820c495686dcd584c9e254c547b0
vann <vaniamelia12@students.untidar.ac.id>
2024-08-30 14:33:39 +0700

    Bekerja dengan Java

---
+++ VaniatugasPrakPBO2.java
@@ -0,0 +1,21 @@
+// File: src/main/java/com/mycompany/vaniatugas2/Vaniatugas2.java
+package com.mycompany.vaniatugas2;
+
+public class Vaniatugas2 {
+    public static void main(String[] args) {
+        // Variabel untuk menyimpan informasi
+        String nama = "Vania Amelia Setya Wijaya";
+        String npm = "2330506068";
+        String alamat = "NAbinKulon, Purworejo, Candimulyo, Magelang ";
+        String noHP = "085879623284";
+        
+        // Menampilkan informasi
+        System.out.println("• Nama : " + nama);
+        System.out.println("• NPM : " + npm);
+        System.out.println("• Alamat : " + alamat);
+        System.out.println("• No_HP : " + noHP);
+    }
+}
+
+
+
